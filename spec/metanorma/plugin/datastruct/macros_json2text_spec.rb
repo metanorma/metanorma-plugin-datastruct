51ac545f52870b96b29341c2ad0c14f1e61c762d
Тараскин Михаил <MITaraskin@domclick.ru>
2020-10-01 14:17:03 +0300

    #1 use metanorma-standoc for tests, remove Gemfile.lock

--- spec/metanorma/plugin/datastruct/macros_json2text_spec.rb
+++ spec/metanorma/plugin/datastruct/macros_json2text_spec.rb
@@ -2,11 +2,6 @@ require "spec_helper"
 require "metanorma/plugin/datastruct/json2_text_preprocessor"
 
 RSpec.describe Metanorma::Plugin::Datastruct::Json2TextPreprocessor do
-  before do
-    Asciidoctor::Extensions.register do
-      preprocessor Metanorma::Plugin::Datastruct::Json2TextPreprocessor
-    end
-  end
   it_behaves_like "structured data 2 text preprocessor" do
     let(:extention) { "json" }
     def transform_to_type(data)
