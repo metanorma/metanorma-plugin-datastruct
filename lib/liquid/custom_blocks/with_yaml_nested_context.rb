58b9374078ee9345c3492184c07a31703e614473
MikhailTretyakov <8926238@gmail.com>
2021-05-17 10:29:02 +0300

    https://github.com/metanorma/metanorma-plugin-datastruct/issues/11 fix nested yaml blocks yaml repeated nodes, specs refactor

--- lib/liquid/custom_blocks/with_yaml_nested_context.rb
+++ lib/liquid/custom_blocks/with_yaml_nested_context.rb
@@ -10,7 +10,9 @@ module Liquid
         context_file = context[@context_file_variable].to_s.strip
         context[@context_name] = YAML.safe_load(
           File.read(context_file, encoding: "utf-8"),
-          [Date, Time]
+          [Date, Time],
+          [],
+          true
         )
         super
       end
--- spec/metanorma/plugin/datastruct/macros_yaml2text_spec.rb
+++ spec/metanorma/plugin/datastruct/macros_yaml2text_spec.rb
@@ -11,49 +11,69 @@ RSpec.describe Metanorma::Plugin::Datastruct::Yaml2TextPreprocessor do
 
   context 'when YAML repeated nodes' do
     let(:example_file) { assets_path('nested_repeated_nodes.yml') }
+    let(:example_file_2) { assets_path('nested_repeated_nodes_2.yml') }
+    let(:input) do
+      <<~TEXT
+        = Document title
+        Author
+        :docfile: test.adoc
+        :nodoc:
+        :novalid:
+        :no-isobib:
+        :imagesdir: spec/assets
 
-    context "Array of hashes" do
+        [yaml2text,#{example_file},data]
+        ----
+        === Nicaragua
 
-      let(:input) do
-        <<~TEXT
-          = Document title
-          Author
-          :docfile: test.adoc
-          :nodoc:
-          :novalid:
-          :no-isobib:
-          :imagesdir: spec/assets
+        Amateur stations:: {{ data.groups.amateur.regex }}
 
-          [yaml2text,#{example_file},data]
-          ----
-          === Findland
 
-          Amateur stations and experimental stations:: {{ data.groups.amateur.regex }}
-          ----
-        TEXT
-      end
-      let(:output) do
-        <<~TEXT
-          #{BLANK_HDR}
-          <sections>
-          <clause id="_" inline-header="false" obligation="normative">
-          <title>Findland</title>
-          <dl id="_">
-          <dt>Amateur stations and experimental stations</dt>
-          <dd>
-          <p id="_">O[F-J][:digit:][0-9A-Z]{3}[:upper:]{1}</p>
-          </dd>
-          </dl>
-          </clause>
-          </sections>
-          </standard-document>
-        TEXT
-      end
+        [yaml2text,#{example_file_2},data_two]
+        ---
+        === Niger
 
-      it "correctly renders input" do
-        expect(xml_string_conent(metanorma_process(input)))
-          .to(be_equivalent_to(output))
-      end
+        Amateur stations:: {{ data_two.groups.amateur.regex }}
+
+        Experimental:: {{ data_two.groups.experimental }}
+        ---
+        ----
+      TEXT
+    end
+    let(:output) do
+      <<~TEXT
+        #{BLANK_HDR}
+        <sections>
+        <clause id="_" inline-header="false" obligation="normative">
+        <title>Nicaragua</title>
+        <dl id="_">
+        <dt>Amateur stations</dt>
+        <dd>
+        <p id="_">O[F-J][:digit:][0-9A-Z]{3}[:upper:]{1}</p>
+        </dd>
+        </dl>
+        </clause>
+        <clause id="_" inline-header="false" obligation="normative">
+        <title>Niger</title>
+        <dl id="_">
+        <dt>Amateur stations</dt>
+        <dd>
+        <p id="_">O[F-J][:upper:]{5,10}</p>
+        </dd>
+        <dt>Experimental</dt>
+        <dd>
+        <p id="_">{&#8221;regex&#8221;&#8658;&#8221;O[F-J][:upper:]{5,10}&#8221;}</p>
+        </dd>
+        </dl>
+        </clause>
+        </sections>
+        </standard-document>
+      TEXT
+    end
+
+    it "correctly renders input" do
+      expect(xml_string_conent(metanorma_process(input)))
+        .to(be_equivalent_to(output))
     end
   end
 end
