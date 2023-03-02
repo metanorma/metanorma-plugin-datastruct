e87690ae4a58af526c505c7149ccf300ee8169c9
KW Kwan <kw.kwan@ribose.com>
2023-03-02 12:08:04 +0800

    Add filter to perform regex replace

---
+++ lib/liquid/custom_filters/replace_regex.rb
@@ -0,0 +1,8 @@
+module Liquid
+  module CustomFilters
+    def replace_regex(text, regex_search, replace_value)
+      regex = /#{regex_search}/
+      text.to_s.gsub(regex, replace_value)
+    end
+  end
+end
